2c65d55613401ceefaa7f6e54c1f9e3f640ec4a7
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-06-04 16:52:28 +0100

    fix:use client to login page

--- src/app/(routes)/(admin)/dashboard/page.tsx
+++ src/app/(routes)/(admin)/dashboard/page.tsx
@@ -11,6 +11,23 @@ function Page() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+const merchantData=[
+  {
+    id:1,
+    value:10,
+    title:"Total orders",
+    icon:"",
+    bg:"#1B9CF9"
+  },
+  {
+    id:2,
+    value:20,
+    title:"Pending orders",
+    icon:<FiClock size={30} />,
+    bg:"#F9A21B"
+  }
+]
+
   return (
     <div>
       <Sidenav isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
@@ -21,14 +38,19 @@ function Page() {
             <div className='text-2xl font-bold my-4'>Merchant Dashboard</div>
            <div className='md:grid grid-cols-3 gap-5 bg-white rounded-lg shadow-lg p-3'>
            {
-                [1,2,3].map((val,i)=>
-                  <div className='bg-[#1B9CF9] p-5 rounded-lg text-white'>
-                    <FiClock size={30} />
-                    <div className='font-bold text-3xl'>10</div>
-                    <div>Total orders</div>
+                merchantData.map((val,i)=>
+                  <div className={`bg-[${val.bg}] p-5 rounded-lg text-white`}>
+                   {val.icon}
+                    <div className='font-bold text-5xl'>{val.value}</div>
+                    <div>{val.title}</div>
                   </div>
                 )
             }
+             <div className='bg-[#3AF56F] p-5 rounded-lg'>
+                  
+                    <div className=' text-xl text-[#656464]'>Numbers of sale</div>
+                    <div className='text-3xl mt-8 font-bold'>$100,000</div>
+                  </div>
            </div>
             <div>
               <Image src={require("../../../../assets/graph.png")} alt={''}/>
