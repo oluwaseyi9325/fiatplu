62ee15c112a5b8967474ac1c5e9824e1ef81e02f
Adeolu Amole <adeoluamole@gmail.com>
2024-06-06 10:50:13 +0100

    formatted and commented the login, signup, and settings page as well as refactoring the order page

--- src/app/(routes)/(admin)/orders/page.tsx
+++ src/app/(routes)/(admin)/orders/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import Header from '@/app/components/Header';
-import Sidenav from '@/app/components/Sidenav';
-import React, { useState } from 'react';
+import Header from "@/app/components/Header";
+import Sidenav from "@/app/components/Sidenav";
+import React, { useState } from "react";
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { CiMenuKebab } from "react-icons/ci";
 
 function Page() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -21,111 +21,130 @@ function Page() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  
-
   return (
     <div className="flex flex-col min-h-screen">
       <Sidenav isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <Header toggleSidebar={toggleSidebar} />
       <div className="flex-1 ml-0 md:ml-64 pt-16 transition-all">
-        <section className="lg:p-[80px] p-4 mt-5 flex flex-col lg:flex-row gap-6">
+        <section className="lg:p-[80px] p-4 mt-5 flex flex-col lg:flex-row gap-6 text-customTextColor">
           <div className="flex-1">
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-              <div className='bg-white p-6 rounded-lg text-black flex flex-col items-start'>
-                <div className='text-xl'>Total Oders</div>
-                <div className='text-xl mt-8'>Total of 48 active orders</div>
-                <div className='bg-[#F9A21B] mt-2 px-20 py-2 rounded-lg shadow text-white text-center text-1xl'>SEE ALL</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="bg-white p-6 rounded-lg text-black flex flex-col items-start relative">
+                <div className="text-[20px] leading-[32px] tracking-[0.15px] text-poppins font-[500] text-customTextColor">
+                  Total Oders
+                </div>
+                <div className="text-[14px] mt-[6px] text-poppins font-[500] text-customTextColor">
+                  Total of <span className="text-[#56CA00]">48</span> active 😍
+                  orders
+                </div>
+                <button className="bg-[#F9A21B] mt-[17px] px-20 py-2 rounded-md shadow text-white text-center text-1xl">
+                  SEE ALL
+                </button>
+                <CiMenuKebab size={18} className="absolute  right-2" />
               </div>
-            
-                <div  className={`bg-white p-6 rounded-lg text-black flex flex-col items-start`}>
-                  <div className='bg-[#56CA00] p-8 rounded-full'></div>
-                  <div className='mt-1'>Status: Active</div>
-                  <div className='flex gap-4 items-center'>
-                  <div className='font-bold text-5xl '>453</div>
-                  <div className='mt-1'>Total orders</div>
+
+              <div
+                className={`bg-white p-6 rounded-lg text-black flex flex-col items-start relative `}
+              >
+                <div className="bg-[#56CA00] p-8 rounded-full"></div>
+                <div className="mt-[17px] text-[15px] leading-[24px] tracking-[0.31px] text-poppins font-[500] text-customTextColor">
+                  Status: Active
+                </div>
+                <div className="flex gap-4 items-center">
+                  <div className="font-bold text-[22px] text-customTextColor">
+                    453
+                  </div>
+                  <div className="text-[15px] leading-[24px] tracking-[0.31px] text-poppins font-[500] text-customTextColor">
+                    Total orders
                   </div>
                 </div>
-                <div  className={`bg-white p-6 rounded-lg text-black flex flex-col items-start`}>
-                  <div className='bg-[#CA1800] p-8 rounded-full'></div>
-                  <div className='mt-1'>Status: Active</div>
-                  <div className='flex gap-4 items-center'>
-                  <div className='font-bold text-5xl'>453</div>
-                  <div className='mt-1'>Total orders</div>
+                <CiMenuKebab size={18} className="absolute  right-2" />
+              </div>
+              <div
+                className={`bg-white p-6 rounded-lg text-black flex flex-col items-start relative`}
+              >
+                <div className="bg-[#CA1800] p-8 rounded-full"></div>
+                <div className="mt-[17px] text-[15px] leading-[24px] tracking-[0.31px] text-poppins font-[500] text-customTextColor">
+                  Status: Stopped
+                </div>
+                <div className="flex gap-4 items-center">
+                  <div className="font-bold text-[22px] text-customTextColor">
+                    400
+                  </div>
+                  <div className="text-[15px] leading-[24px] tracking-[0.31px] text-poppins font-[500] text-customTextColor">
+                    Total orders
                   </div>
                 </div>
-            
+                <CiMenuKebab size={18} className="absolute  right-2" />
+              </div>
             </div>
 
             <section className=" flex flex-col lg:flex-row gap-6 mt-3">
               <div className="flex-1 bg-white rounded-lg shadow p-6">
-                <div className='mb-6'>
-                  <div className='text-xl font-bold'>Create new order</div>
-                  <div className='text-gray-600'>Create, edit, and publish your order</div>
-                </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                  <div>
-                  <Select>
-      <SelectTrigger className=" bg-white">
-        <SelectValue placeholder="Select order type" />
-      </SelectTrigger>
-      <SelectContent className='bg-white'>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-                   
-                  </div>
-                  <div>
-                    
-                  <Select>
-      <SelectTrigger className=" bg-white">
-        <SelectValue placeholder="Select giftcard type" />
-      </SelectTrigger>
-      <SelectContent className='bg-white'>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+                <div className="mb-6">
+                  <div className="text-xl font-bold">Create new order</div>
+                  <div className="text-gray-600">
+                    Create, edit, and publish your order
                   </div>
                 </div>
-                <div className='mt-6'>
-                 
-                <Select>
-      <SelectTrigger className=" bg-white">
-        <SelectValue placeholder="Input amount range" />
-      </SelectTrigger>
-      <SelectContent className='bg-white'>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <div>
+                    <Select>
+                      <SelectTrigger className=" bg-white">
+                        <SelectValue placeholder="Select order type" />
+                      </SelectTrigger>
+                      <SelectContent className="bg-white">
+                        <SelectGroup>
+                          <SelectLabel>Fruits</SelectLabel>
+                          <SelectItem value="apple">Apple</SelectItem>
+                          <SelectItem value="banana">Banana</SelectItem>
+                          <SelectItem value="blueberry">Blueberry</SelectItem>
+                          <SelectItem value="grapes">Grapes</SelectItem>
+                          <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div>
+                    <Select>
+                      <SelectTrigger className=" bg-white">
+                        <SelectValue placeholder="Select giftcard type" />
+                      </SelectTrigger>
+                      <SelectContent className="bg-white">
+                        <SelectGroup>
+                          <SelectLabel>Fruits</SelectLabel>
+                          <SelectItem value="apple">Apple</SelectItem>
+                          <SelectItem value="banana">Banana</SelectItem>
+                          <SelectItem value="blueberry">Blueberry</SelectItem>
+                          <SelectItem value="grapes">Grapes</SelectItem>
+                          <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
                 </div>
-               
+                <div className="mt-6">
+                  <Select>
+                    <SelectTrigger className=" bg-white">
+                      <SelectValue placeholder="Input amount range" />
+                    </SelectTrigger>
+                    <SelectContent className="bg-white">
+                      <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
               </div>
             </section>
 
             <section>
-                <table>
-
-                </table>
+              <table></table>
             </section>
           </div>
         </section>
@@ -135,4 +154,3 @@ function Page() {
 }
 
 export default Page;
-
--- src/app/(routes)/(admin)/settings/page.tsx
+++ src/app/(routes)/(admin)/settings/page.tsx
@@ -1,13 +1,43 @@
-"use client"
+"use client";
 
-import Sidenav from '@/app/components/Sidenav';
-import Image from 'next/image';
-import React, { useState } from 'react';
+import Sidenav from "@/app/components/Sidenav";
+import Image from "next/image";
+import React, { useState } from "react";
 import { FiClock } from "react-icons/fi";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function Page() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const validationSchema = Yup.object({
+    firstname: Yup.string().required("First Name is required"),
+    lastname: Yup.string().required("Last Name is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    phonenumber: Yup.string()
+      .matches(/^[0-9]+$/, "Must be only digits")
+      .required("Phone number is required"),
+    address: Yup.string().required("Address is required"),
+    state: Yup.string().required("State of Origin is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phonenumber: "",
+      address: "",
+      state: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log("Form data and ", values);
+    },
+  });
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -19,113 +49,195 @@ function Page() {
       value: 10,
       title: "Total orders",
       icon: <IoMdCheckmarkCircleOutline size={30} />,
-      bg: "bg-blue-500"
+      bg: "bg-blue-500",
     },
     {
       id: 2,
       value: 20,
       title: "Pending orders",
       icon: <FiClock size={30} />,
-      bg: "bg-yellow-500"
-    }
+      bg: "bg-yellow-500",
+    },
   ];
 
   return (
     <div className="flex flex-col min-h-screen">
       <Sidenav isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <Header toggleSidebar={toggleSidebar} />
-      <div className="flex-1 ml-0 md:ml-64 pt-16 transition-all">
+      <div className="flex-1 ml-0 md:ml-64 pt-24 transition-all">
         <section className="p-6 flex flex-col lg:flex-row gap-6 mt-3">
           <div className="flex-1 bg-white rounded-lg shadow p-6">
-            <div className='flex gap-3 mt-5'>
-                <Image alt='' src={require("../../../../assets/avatar.png")} />
-                <div>
-                    <div>James Drake</div>
-                    <div className='text-[#F9A21B] font-bold'>Change profile picture</div>
+            <div className="flex gap-3 mt-5">
+              <div className="relative h-[50px] w-[50px]">
+              <Image alt="user profile" src={require("../../../../assets/avatar.png")} className="" layout="fill"
+                objectFit="fill" />
+              </div>
+              <div>
+                <div className="leading-[30px] text-[20px] text-poppins font-[500]">James Drake</div>
+                <div className="text-[#F9A21B] font-[500] tetx-[15px]">
+                  Change profile picture
                 </div>
-            </div>
-            <div className='lg:flex justify-between gap-3 mt-5'>
-                <div>
-                <div className='mt-4'>
-                    <label htmlFor="password" className="block font-medium">Fullname</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your Fullname"
-                        className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                </div>
-                <div className='mt-4'>
-                    <label htmlFor="password" className="block font-medium">Email</label>
-                    <input
-                        id="password"
-                        type="Email"
-                        placeholder="Enter your Email"
-                        className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                </div>
-                <div className='mt-4'>
-                    <label htmlFor="password" className="block font-medium">Address</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your Address"
-                        className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                </div>
-                </div>
-                <div>
-                <div className='mt-4'>
-                    <label htmlFor="password" className="block font-medium">Last Name</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                </div>
-                <div className='mt-4'>
-                    <label htmlFor="password" className="block font-medium">Phone Number</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                </div>
-                <div className='mt-4'>
-                    <label htmlFor="password" className="block font-medium">State of Origin</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        className="w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                    />
-                </div>
-                </div>
-            </div>
-            <div className='flex mt-10'><div className='bg-[#F9A21B] p-2 rounded-lg text-white text-center  cursor-pointer'>
-               Update
-              </div></div>
-          </div>
-          <div className='flex-1 lg:flex-none lg:w-1/3'>
-          <div className="">
-            <div className='bg-white p-10 rounded-lg shadow'>
-              <div className='text-2xl font-bold mb-4'>Change password</div>
-              <div>You can change your current password to ensure account is well secured</div>
-              <div className='bg-[#FFF5E4] p-3 rounded-lg text-[#F9A21B] text-center mt-4 cursor-pointer'>
-              Change password
               </div>
             </div>
-            <div className='bg-white p-10 rounded-lg shadow mt-4'>
-              <div className='text-2xl font-bold mb-4'>Delete account</div>
-              <div>You can change your current password to ensure account is well secured</div>
-              <div className='bg-[#FFF5E4] p-3 rounded-lg text-[#F9A21B] text-center mt-4 cursor-pointer'>
-              Delete account
+            <form
+              onSubmit={formik.handleSubmit}
+              className="w-full flex flex-col lg:flex-row flex-wrap mt-[30px] md:gap-x-[25px] text-customTextColor"
+            >
+              <div className="mt-4 w-full md:w-[48%]">
+                <label htmlFor="firstname" className="block font-medium text-[12px]">
+                  First Name
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  {...formik.getFieldProps("firstname")}
+                  placeholder="Enter your First Name"
+                  className={`w-full px-4 py-2 mt-1 border rounded-md focus:outline-none ${
+                    formik.touched.firstname && formik.errors.firstname
+                      ? "border-customOrangeTextColor"
+                      : ""
+                  }`}
+                />
+                {formik.touched.firstname && formik.errors.firstname ? (
+                  <p className="text-red-500 text-sm">
+                    {formik.errors.firstname}
+                  </p>
+                ) : null}
+              </div>
+              <div className="mt-4 w-full md:w-[48%]">
+                <label htmlFor="lastname" className="block font-medium text-[12px]">
+                  Last Name
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  {...formik.getFieldProps("lastname")}
+                  placeholder="Enter your Last Name"
+                  className={`w-full px-4 py-2 mt-1 border rounded-md focus:outline-none ${
+                    formik.touched.lastname && formik.errors.lastname
+                      ? "border-customOrangeTextColor"
+                      : ""
+                  }`}
+                />
+                {formik.touched.lastname && formik.errors.lastname ? (
+                  <p className="text-red-500 text-sm">
+                    {formik.errors.lastname}
+                  </p>
+                ) : null}
+              </div>
+              <div className="mt-4 w-full md:w-[48%]">
+                <label htmlFor="email" className="block font-medium text-[12px]">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  {...formik.getFieldProps("email")}
+                  placeholder="Enter your Email"
+                  className={`w-full px-4 py-2 mt-1 border rounded-md focus:outline-none ${
+                    formik.touched.email && formik.errors.email
+                      ? "border-customOrangeTextColor"
+                      : ""
+                  }`}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <p className="text-red-500 text-sm">{formik.errors.email}</p>
+                ) : null}
+              </div>
+              <div className="mt-4 w-full md:w-[48%]">
+                <label htmlFor="phonenumber" className="block font-medium text-[12px]">
+                  Phone Number
+                </label>
+                <input
+                  id="phonenumber"
+                  type="text"
+                  {...formik.getFieldProps("phonenumber")}
+                  placeholder="Enter your phone number"
+                  className={`w-full px-4 py-2 mt-1 border rounded-md focus:outline-none ${
+                    formik.touched.phonenumber && formik.errors.phonenumber
+                      ? "border-customOrangeTextColor"
+                      : ""
+                  }`}
+                />
+                {formik.touched.phonenumber && formik.errors.phonenumber ? (
+                  <p className="text-red-500 text-sm">
+                    {formik.errors.phonenumber}
+                  </p>
+                ) : null}
+              </div>
+              <div className="mt-4 w-full md:w-[48%]">
+                <label htmlFor="address" className="block font-medium text-[12px]">
+                  Address
+                </label>
+                <input
+                  id="address"
+                  type="text"
+                  {...formik.getFieldProps("address")}
+                  placeholder="Enter your Address"
+                  className={`w-full px-4 py-2 mt-1 border rounded-md focus:outline-none ${
+                    formik.touched.address && formik.errors.address
+                      ? "border-customOrangeTextColor"
+                      : ""
+                  }`}
+                />
+                {formik.touched.address && formik.errors.address ? (
+                  <p className="text-red-500 text-sm">
+                    {formik.errors.address}
+                  </p>
+                ) : null}
+              </div>
+              <div className="mt-4 w-full md:w-[48%]">
+                <label htmlFor="state" className="block font-medium text-[12px]">
+                  State of Origin
+                </label>
+                <input
+                  id="state"
+                  type="text"
+                  {...formik.getFieldProps("state")}
+                  placeholder="Enter your state"
+                  className={`w-full px-4 py-2 mt-1 border rounded-md focus:outline-none ${
+                    formik.touched.state && formik.errors.state
+                      ? "border-customOrangeTextColor"
+                      : ""
+                  }`}
+                />
+                {formik.touched.state && formik.errors.state ? (
+                  <p className="text-red-500 text-sm">{formik.errors.state}</p>
+                ) : null}
+              </div>
+              <div className="flex mt-10 w-full">
+                <button
+                  type="submit"
+                  className="bg-customOrangeBackground py-2 px-10 w-full md:w-[unset] rounded-lg text-white text-center leading-[21px] font-poppins text-[14px] cursor-pointer hover:bg-customOrangeBackgroundDarker"
+                >
+                  Update
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="flex-1 lg:flex-none lg:w-1/3">
+            <div className="">
+              <div className="bg-white p-10 rounded-lg shadow">
+                <div className="text-[20px] text-poppins leading-[30px] font-bold mb-4">Change password</div>
+                <div className="leading-[19.5px] text-[13px] font-[400] text-poppins">
+                  You can change your current password to ensure account is well
+                  secured
+                </div>
+                <button className="bg-[#FFF5E4] p-3 rounded-lg leading-[22.5px] w-full text-[#F9A21B] text-center mt-4 cursor-pointer">
+                  Change password
+                </button>
+              </div>
+              <div className="bg-white p-10 rounded-lg shadow mt-4">
+                <div className="text-[20px] text-poppins leading-[30px] font-bold mb-4">Delete account</div>
+                <div className="leading-[19.5px] text-[13px] font-[400] text-poppins">
+                  You can change your current password to ensure account is well
+                  secured
+                </div>
+                <div className="bg-[#FFF5E4] p-3 rounded-lg leading-[22.5px] w-full text-[#F9A21B] text-center mt-4 cursor-pointer">
+                  Delete account
+                </div>
               </div>
             </div>
-            
-          </div>
           </div>
         </section>
       </div>
@@ -135,20 +247,26 @@ function Page() {
 
 export default Page;
 
-
-
-const Header = ({ toggleSidebar }:any) => {
+const Header = ({ toggleSidebar }: any) => {
   return (
     <div className="fixed top-0 left-0 md:left-64 right-0 h-20 bg-white  items-center px-6 shadow-md flex   justify-between">
       <button onClick={toggleSidebar} className="md:hidden mr-4">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
         </svg>
       </button>
-      <div className='md:text-4xl text-2xl font-bold'>Account Setting</div>
+      <div className="md:text-4xl text-2xl font-bold">Account Setting</div>
     </div>
   );
 };
-
-
-
